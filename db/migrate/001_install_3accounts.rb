ef726a255dc3ebd4a74f4bc7d0815b5ca224a728
Neil Wilson <neil@aldur.co.uk>
2007-09-02 09:26:37 +0000

    Update account migrations to latest Rails datatypes

--- db/migrate/001_install_3accounts.rb
+++ db/migrate/001_install_3accounts.rb
@@ -1,22 +1,24 @@
 class Install3accounts < ActiveRecord::Migration
   def self.up
     create_table "accounts", :force => true do |t|
-  	t.column "name", :string
+  	t.column :name, :string
+   	t.column :created_at,     :datetime
+   	t.column :updated_at,     :datetime
     end
 
     create_table "categories", :force => true do |t|
-  	t.column "name", :string
+  	t.column :name, :string
+   	t.column :created_at,     :datetime
+   	t.column :updated_at,     :datetime
     end
 
     create_table "entries", :force => true do |t|
-    	t.column "account_id",     :integer
-    	t.column "category_id",    :integer
-    	t.column "effective_date", :date
-    	t.column "checked",        :boolean
-    	t.column "check_number",   :string
-    	t.column "comment",        :string
-    	t.column "value",          :float
-   	t.column "created_at",     :datetime
+    	t.column :account_id,     :integer
+    	t.column :category_id ,    :integer
+    	t.column :comment,        :string
+    	t.column :value,          :decimal, :precision => 8, :scale => 2
+   	t.column :created_at,     :datetime
+   	t.column :updated_at,     :datetime
     end
   end
 
