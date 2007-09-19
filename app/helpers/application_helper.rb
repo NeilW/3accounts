59253d4bfacb12066b90a187a49419ca11032bcb
Neil Wilson <neil@aldur.co.uk>
2007-09-19 10:07:48 +0000

    Add AGL comments to created files. Update formatting. Add and update donation page. Update menus for static pages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,19 @@
+# 3Accounts - Accounts management and compliance
+# Copyright (C) 2007 Neil Wilson, Aldur Systems Ltd 
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 module ApplicationHelper
   
   # Show the local time
@@ -7,7 +23,7 @@ module ApplicationHelper
   
   # The URL of the discussion group.
   def group_url
-    "http://groups.google.com/groups/3accounts"
+    "http://groups.google.com/group/3accounts"
   end
 
   # The URL of the project management page
