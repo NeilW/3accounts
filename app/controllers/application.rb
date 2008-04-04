f5da6dfdd842e19bea8c6fd3911802f216eb9114
Neil Wilson <neil@aldur.co.uk>
2008-04-04 15:13:25 +0100

    Initial files

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '0a8eb78c623470ef29b2b6d86817f846'
+end
